b4c6feb3335b3b63b4b2b5146198db3996228db8
James Chambers <james@jameschambers.com>
2015-08-11 16:01:21 -0500

    MVC 6 Project Pivot
    Initial project creation
    - changed framework to dnx46
    - added reference to Core
    - added views in Home, ExpenseReport, ExpenseReportSearch and ToDo
    - weaved changes in to Shared views (more complicated than just a copy)
    - stitched in the controllers, models, helper classes and other pieces -
    mostly namespace issues
    - added in the static content - will come back to this

---
+++ src/ClearMeasure.Bootcamp.UI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// using ClearMeasure.Bootcamp.UI.Helpers.ActionFilters;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace ClearMeasure.Bootcamp.UI.Controllers
+{
+    // todo: target for MVC6 rework
+    //[AddUserMetaDataToViewData]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ClearMeasure.Bootcamp.UI/Controllers/NavigationController.cs
@@ -0,0 +1,28 @@
+﻿using ClearMeasure.Bootcamp.Core.Services;
+using Microsoft.AspNet.Mvc;
+using UI.Models;
+
+namespace ClearMeasure.Bootcamp.UI.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private readonly IUserSession _session;
+
+        public NavigationController(IUserSession session)
+        {
+            _session = session;
+        }
+
+        // todo: target for MVC6 rework
+        //[ChildActionOnly]
+        public ActionResult Menu()
+        {
+            var currentUser = _session.GetCurrentUser();
+
+            var model = new NavigationMenuModel();
+
+            return PartialView(model);
+        }
+
+    }
+}
