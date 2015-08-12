5209f711a47714cb680ec177dc1156ff0c4f3518
James Chambers <james@jameschambers.com>
2015-08-12 14:57:01 -0500

    Application is RUNNING
    - fixed view import errors (not using type)
    - tested DI on Home Controller
    - starting to weave together the bits to make it come together!s

--- src/ClearMeasure.Bootcamp.UI/Controllers/HomeController.cs
+++ src/ClearMeasure.Bootcamp.UI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿// using ClearMeasure.Bootcamp.UI.Helpers.ActionFilters;
+
+using ClearMeasure.Bootcamp.UI.Models.SelectListProviders;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 
@@ -6,9 +8,16 @@ namespace ClearMeasure.Bootcamp.UI.Controllers
 {
     // todo: target for MVC6 rework -action filter
     //[AddUserMetaDataToViewData]
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
+        private readonly UserSelectListProvider _provider;
+
+        public HomeController(UserSelectListProvider provider)
+        {
+            _provider = provider;
+        }
+
         public ActionResult Index()
         {
             return View();
